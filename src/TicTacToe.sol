72101e76773a449323bbeb122a2a52c797606f67
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-10 19:41:33 -0400

    fix: add open function definition to TicTacToe

--- src/TicTacToe.sol
+++ src/TicTacToe.sol
@@ -3,4 +3,6 @@ pragma solidity 0.8.29;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
-contract TicTacToe is EIP712("Tic-Tac-Toe", "1") {}
+contract TicTacToe is EIP712("Tic-Tac-Toe", "1") {
+    function open(address alice, address bob, uint256 deadline, uint8 timeout, bytes32 r, bytes32 vs) external {}
+}
