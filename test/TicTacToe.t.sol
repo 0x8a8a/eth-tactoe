d63bcb175243aef3c254b388b676c7c6b18dde50
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-10 17:53:56 -0400

    fix: add account and contract fixtures to TicTacToeBaseTest

--- test/TicTacToe.t.sol
+++ test/TicTacToe.t.sol
@@ -3,4 +3,23 @@ pragma solidity 0.8.29;
 
 import {Test} from "forge-std/Test.sol";
 
-contract TicTacToeBaseTest is Test {}
+import {TicTacToe} from "src/TicTacToe.sol";
+
+contract TicTacToeBaseTest is Test {
+    address public alice;
+    uint256 public alicePk;
+
+    address public bob;
+    uint256 public bobPk;
+
+    TicTacToe public tictactoe;
+
+    constructor() {
+        (alice, alicePk) = makeAddrAndKey("alice");
+        (bob, bobPk) = makeAddrAndKey("bob");
+    }
+
+    function setUp() public {
+        tictactoe = new TicTacToe();
+    }
+}
