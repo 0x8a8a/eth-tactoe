9b0f52b351f46ad0579e501fc13acac9d19880e4
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-10 22:02:53 -0400

    fix: inherit Multicall

--- src/TicTacToe.sol
+++ src/TicTacToe.sol
@@ -2,7 +2,8 @@
 pragma solidity 0.8.29;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
 
-contract TicTacToe is EIP712("Tic-Tac-Toe", "1") {
+contract TicTacToe is EIP712("Tic-Tac-Toe", "1"), Multicall {
     function open(address alice, address bob, uint256 deadline, uint8 timeout, bytes32 r, bytes32 vs) external {}
 }
