d1fd59a50c6c751ef6049f7342938d658e61e700
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 19:50:05 -0400

    fix: revert with UnauthorizedCaller in open function

--- src/TicTacToe.sol
+++ src/TicTacToe.sol
@@ -5,5 +5,9 @@ import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
 
 contract TicTacToe is EIP712("Tic-Tac-Toe", "1"), Multicall {
-    function open(address alice, address bob, uint256 deadline, uint8 timeout, bytes32 r, bytes32 vs) external {}
+    error UnauthorizedCaller(address caller);
+
+    function open(address alice, address bob, uint256 deadline, uint8 timeout, bytes32 r, bytes32 vs) external {
+        require(msg.sender == alice || msg.sender == bob, UnauthorizedCaller(msg.sender));
+    }
 }
