be4b070601db8acd49687a82b68aeb7617ea5a27
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 19:48:54 -0400

    test: open reverts with UnauthorizedCaller

--- test/TicTacToe.open.t.sol
+++ test/TicTacToe.open.t.sol
@@ -3,4 +3,15 @@ pragma solidity 0.8.29;
 
 import {TicTacToeBaseTest} from "test/TicTacToe.t.sol";
 
-contract TicTacToeOpenTest is TicTacToeBaseTest {}
+contract TicTacToeOpenTest is TicTacToeBaseTest {
+    error UnauthorizedCaller(address caller);
+
+    function testFuzz_RevertsIf_CallerIsUnauthorized(address caller) public {
+        vm.assume(caller != alice && caller != bob);
+
+        vm.expectRevert(abi.encodeWithSelector(UnauthorizedCaller.selector, caller));
+
+        vm.prank(caller);
+        tictactoe.open(alice, bob, 0, 0, bytes32(0), bytes32(0));
+    }
+}
