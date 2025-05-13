19b1aaca3e0e2f096798ec69fbb2ba82c7f2661e
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 22:27:25 -0400

    fix: revert with UnauthorizedSigner in open function

--- src/TicTacToe.sol
+++ src/TicTacToe.sol
@@ -1,18 +1,31 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.29;
 
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
 
+import {LibSigUtils} from "src/LibSigUtils.sol";
+
 contract TicTacToe is EIP712("Tic-Tac-Toe", "1"), Multicall {
+    using LibSigUtils for *;
+
+    mapping(address alice => mapping(address bob => uint256)) public nonces;
+
     error UnauthorizedCaller(address caller);
     error ExpiredSignature(uint256 deadline);
     error InvalidTimeout(uint8 timeout);
+    error UnauthorizedSigner(address signer);
 
     function open(address alice, address bob, uint256 deadline, uint8 timeout, bytes32 r, bytes32 vs) external {
         require(msg.sender == alice || msg.sender == bob, UnauthorizedCaller(msg.sender));
         // slither-disable-next-line timestamp
         require(block.timestamp <= deadline, ExpiredSignature(deadline));
         require(timeout >= 60, InvalidTimeout(timeout));
+
+        uint256 id = nonces[alice][bob]++;
+        bytes32 structHash = LibSigUtils.Open(LibSigUtils.Channel(alice, bob, id), deadline, timeout).hash();
+        address signer = ECDSA.recover(_hashTypedDataV4(structHash), r, vs);
+        require(signer == (msg.sender == alice ? bob : alice), UnauthorizedSigner(signer));
     }
 }
