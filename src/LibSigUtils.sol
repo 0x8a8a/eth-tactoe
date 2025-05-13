de63eee54c6a395a2501bd9753dab351740459dc
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 21:35:08 -0400

    fix: add Open struct to LibSigUtils

--- src/LibSigUtils.sol
+++ src/LibSigUtils.sol
@@ -8,5 +8,13 @@ library LibSigUtils {
         uint256 id;
     }
 
+    struct Open {
+        Channel channel;
+        uint256 deadline;
+        uint8 timeout;
+    }
+
     bytes32 internal constant CHANNEL_TYPEHASH = keccak256("Channel(address alice,address bob,uint256 id)");
+    bytes32 internal constant OPEN_TYPEHASH =
+        keccak256("Open(Channel channel,uint256 deadline,uint8 timeout)Channel(address alice,address bob,uint256 id)");
 }
