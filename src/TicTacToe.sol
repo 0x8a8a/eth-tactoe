325b1d6f63ba342b1a22f829964b9a740f3d644c
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 22:36:11 -0400

    fix: emit the Opened event in the open function

--- src/TicTacToe.sol
+++ src/TicTacToe.sol
@@ -4,13 +4,17 @@ pragma solidity 0.8.29;
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import {LibSigUtils} from "src/LibSigUtils.sol";
 
 contract TicTacToe is EIP712("Tic-Tac-Toe", "1"), Multicall {
     using LibSigUtils for *;
+    using SafeCast for *;
 
     mapping(address alice => mapping(address bob => uint256)) public nonces;
+
+    event Opened(address indexed alice, address indexed bob, uint256 indexed id, uint32 expiry, uint8 timeout);
 
     error UnauthorizedCaller(address caller);
     error ExpiredSignature(uint256 deadline);
@@ -27,5 +31,7 @@ contract TicTacToe is EIP712("Tic-Tac-Toe", "1"), Multicall {
         bytes32 structHash = LibSigUtils.Open(LibSigUtils.Channel(alice, bob, id), deadline, timeout).hash();
         address signer = ECDSA.recover(_hashTypedDataV4(structHash), r, vs);
         require(signer == (msg.sender == alice ? bob : alice), UnauthorizedSigner(signer));
+
+        emit Opened(alice, bob, id, (block.timestamp + timeout).toUint32(), timeout);
     }
 }
