cf9aa6068cac75865a65247fcbc4eb2dcae75b67
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 20:08:54 -0400

    test: open reverts with InvalidTimeout

--- test/TicTacToe.open.t.sol
+++ test/TicTacToe.open.t.sol
@@ -5,6 +5,7 @@ import {TicTacToeBaseTest} from "test/TicTacToe.t.sol";
 
 contract TicTacToeOpenTest is TicTacToeBaseTest {
     error UnauthorizedCaller(address caller);
+    error InvalidTimeout(uint8 timeout);
 
     function testFuzz_RevertsIf_CallerIsUnauthorized(address caller) public {
         vm.assume(caller != alice && caller != bob);
@@ -13,5 +14,14 @@ contract TicTacToeOpenTest is TicTacToeBaseTest {
 
         vm.prank(caller);
         tictactoe.open(alice, bob, 0, 0, bytes32(0), bytes32(0));
+    }
+
+    function test_RevertsIf_TimeoutArgumentIsInvalid() public {
+        for (uint8 timeout = 0; timeout < 60; timeout++) {
+            vm.expectRevert(abi.encodeWithSelector(InvalidTimeout.selector, timeout));
+
+            vm.prank(alice);
+            tictactoe.open(alice, bob, UINT256_MAX, timeout, bytes32(0), bytes32(0));
+        }
     }
 }
