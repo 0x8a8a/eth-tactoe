36f7be4edf05443e7663c2821d61ff805c16bfe3
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 21:38:36 -0400

    fix: add hash functions for the Channel and Open structs

--- src/LibSigUtils.sol
+++ src/LibSigUtils.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.29;
 
 library LibSigUtils {
+    using LibSigUtils for *;
+
     struct Channel {
         address alice;
         address bob;
@@ -17,4 +19,12 @@ library LibSigUtils {
     bytes32 internal constant CHANNEL_TYPEHASH = keccak256("Channel(address alice,address bob,uint256 id)");
     bytes32 internal constant OPEN_TYPEHASH =
         keccak256("Open(Channel channel,uint256 deadline,uint8 timeout)Channel(address alice,address bob,uint256 id)");
+
+    function hash(Channel memory self) internal pure returns (bytes32) {
+        return keccak256(abi.encode(CHANNEL_TYPEHASH, self.alice, self.bob, self.id));
+    }
+
+    function hash(Open memory self) internal pure returns (bytes32) {
+        return keccak256(abi.encode(OPEN_TYPEHASH, self.channel.hash(), self.deadline, self.timeout));
+    }
 }
