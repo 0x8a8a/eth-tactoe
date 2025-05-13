11822c57dcbb71d00e8b0452dd9687e26ad45589
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-12 21:33:18 -0400

    fix: add Channel struct to LibSigUtils

--- src/LibSigUtils.sol
+++ src/LibSigUtils.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.29;
 
-library LibSigUtils {}
+library LibSigUtils {
+    struct Channel {
+        address alice;
+        address bob;
+        uint256 id;
+    }
+
+    bytes32 internal constant CHANNEL_TYPEHASH = keccak256("Channel(address alice,address bob,uint256 id)");
+}
