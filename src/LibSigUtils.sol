792ecc51cc8281bd434b9f50616ee4878fe49241
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-15 16:16:40 -0400

    fix: add Close struct to LibSigUtils with respective hash function

--- src/LibSigUtils.sol
+++ src/LibSigUtils.sol
@@ -16,9 +16,16 @@ library LibSigUtils {
         uint8 timeout;
     }
 
+    struct Close {
+        Channel channel;
+        address winner;
+    }
+
     bytes32 internal constant CHANNEL_TYPEHASH = keccak256("Channel(address alice,address bob,uint256 id)");
     bytes32 internal constant OPEN_TYPEHASH =
         keccak256("Open(Channel channel,uint256 deadline,uint8 timeout)Channel(address alice,address bob,uint256 id)");
+    bytes32 internal constant CLOSE_TYPEHASH =
+        keccak256("Close(Channel channel,address winner)Channel(address alice,address bob,uint256 id)");
 
     function hash(Channel memory self) internal pure returns (bytes32) {
         return keccak256(abi.encode(CHANNEL_TYPEHASH, self.alice, self.bob, self.id));
@@ -26,5 +33,9 @@ library LibSigUtils {
 
     function hash(Open memory self) internal pure returns (bytes32) {
         return keccak256(abi.encode(OPEN_TYPEHASH, self.channel.hash(), self.deadline, self.timeout));
+    }
+
+    function hash(Close memory self) internal pure returns (bytes32) {
+        return keccak256(abi.encode(CLOSE_TYPEHASH, self.channel.hash(), self.winner));
     }
 }
