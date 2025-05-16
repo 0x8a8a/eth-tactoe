19b6542599184c87c83a656fc37b2bbd6b346348
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-16 14:10:12 -0400

    fix(test): add TicTacToeCommitTest contract with setUp

---
+++ test/TicTacToe.commit.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.29;
+
+import {TicTacToeBaseTest} from "test/TicTacToe.t.sol";
+
+import {LibSigUtils} from "src/LibSigUtils.sol";
+
+contract TicTacToeCommitTest is TicTacToeBaseTest {
+    using LibSigUtils for *;
+
+    function setUp() public override {
+        super.setUp();
+
+        bytes32 structHash = LibSigUtils.Open(LibSigUtils.Channel(alice, bob, 0), UINT256_MAX, 60).hash();
+        (bytes32 r, bytes32 vs) = vm.signCompact(bobPk, toTypedDataHash(structHash));
+
+        vm.prank(alice);
+        tictactoe.open(alice, bob, UINT256_MAX, 60, r, vs);
+    }
+}
