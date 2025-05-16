886386ec8fe7c640264caf9b81d8c5f94bddb41f
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-16 12:27:58 -0400

    test: getWinner reverts if called before the channel has expired/closed

--- test/TicTacToe.getWinner.t.sol
+++ test/TicTacToe.getWinner.t.sol
@@ -3,11 +3,27 @@ pragma solidity 0.8.29;
 
 import {TicTacToeBaseTest} from "test/TicTacToe.t.sol";
 
+import {LibSigUtils} from "src/LibSigUtils.sol";
+
 contract TicTacToeGetWinnerTest is TicTacToeBaseTest {
+    using LibSigUtils for *;
+
     error MissingChannel(address alice, address bob, uint256 id);
+    error LiveChannel(uint32 expiry);
 
     function test_RevertsIf_ChannelIsNonexistent() public {
-        vm.expectRevert(abi.encodeWithSelector(MissingChannel.selector, alice, bob, 1));
-        tictactoe.getWinner(alice, bob, 1);
+        vm.expectRevert(abi.encodeWithSelector(MissingChannel.selector, alice, bob, 0));
+        tictactoe.getWinner(alice, bob, 0);
+    }
+
+    function test_RevertsIf_ChannelHasNotExpired() public {
+        bytes32 structHash = LibSigUtils.Open(LibSigUtils.Channel(alice, bob, 0), UINT256_MAX, 60).hash();
+        (bytes32 r, bytes32 vs) = vm.signCompact(bobPk, toTypedDataHash(structHash));
+
+        vm.prank(alice);
+        tictactoe.open(alice, bob, UINT256_MAX, 60, r, vs);
+
+        vm.expectRevert(abi.encodeWithSelector(LiveChannel.selector, tictactoe.getExpiry(alice, bob, 0)));
+        tictactoe.getWinner(alice, bob, 0);
     }
 }
