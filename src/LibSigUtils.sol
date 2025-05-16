a433a2c03f1b301b494bb9de5f8edc6d47c8e962
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-16 13:35:02 -0400

    fix: add Commit struct to LibSigUtils with respective hash function

--- src/LibSigUtils.sol
+++ src/LibSigUtils.sol
@@ -21,11 +21,19 @@ library LibSigUtils {
         address winner;
     }
 
+    struct Commit {
+        Channel channel;
+        uint184 nonce;
+        uint32 states;
+    }
+
     bytes32 internal constant CHANNEL_TYPEHASH = keccak256("Channel(address alice,address bob,uint256 id)");
     bytes32 internal constant OPEN_TYPEHASH =
         keccak256("Open(Channel channel,uint256 deadline,uint8 timeout)Channel(address alice,address bob,uint256 id)");
     bytes32 internal constant CLOSE_TYPEHASH =
         keccak256("Close(Channel channel,address winner)Channel(address alice,address bob,uint256 id)");
+    bytes32 internal constant COMMIT_TYPEHASH =
+        keccak256("Commit(Channel channel,uint184 nonce,uint32 states)Channel(address alice,address bob,uint256 id)");
 
     function hash(Channel memory self) internal pure returns (bytes32) {
         return keccak256(abi.encode(CHANNEL_TYPEHASH, self.alice, self.bob, self.id));
@@ -37,5 +45,9 @@ library LibSigUtils {
 
     function hash(Close memory self) internal pure returns (bytes32) {
         return keccak256(abi.encode(CLOSE_TYPEHASH, self.channel.hash(), self.winner));
+    }
+
+    function hash(Commit memory self) internal pure returns (bytes32) {
+        return keccak256(abi.encode(COMMIT_TYPEHASH, self.channel.hash(), self.nonce, self.states));
     }
 }
