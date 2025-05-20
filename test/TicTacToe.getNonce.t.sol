9936ce7b9a362d8a31521a8309297d2cfe9bfdb5
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-19 20:19:01 -0400

    test: getNonce reverts with InvalidChannel

---
+++ test/TicTacToe.getNonce.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.29;
+
+import {TicTacToeBaseTest} from "test/TicTacToe.t.sol";
+
+contract TicTacToeGetNonceTest is TicTacToeBaseTest {
+    error InvalidChannel(address alice, address bob, uint256 id);
+
+    function test_RevertsIf_ChannelIsNonexistent() public {
+        vm.expectRevert(abi.encodeWithSelector(InvalidChannel.selector, alice, bob, 0));
+        tictactoe.getNonce(alice, bob, 0);
+    }
+}
