caf544c9fcb207bc2464d9327d6dd4959ffe12d1
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-20 18:58:46 -0400

    fix: update containsWin function

--- src/LibLogic.sol
+++ src/LibLogic.sol
@@ -38,7 +38,35 @@ library LibLogic {
         if (bobWon) require(alicePopCount == bobPopCount);
     }
 
+    /**
+     * @dev Checks if the given game state has a winning combination.
+     * This function evaluates the 9-bit integer `state`, representing the game board, and checks
+     * for any possible winning combination in Tic Tac Toe. It does this by performing bitwise AND operations
+     * with predefined values representing the winning positions on the board. If a winning combination is found,
+     * the function returns `true`, otherwise it returns `false`.
+     *
+     * @param state A 9-bit integer representing the state of the Tic Tac Toe board. Each bit corresponds to
+     * a position on the board, with 1 indicating a player's move and 0 indicating an empty space.
+     * The bits are indexed as follows (0-indexed):
+     * ```
+     * [6] [7] [8]
+     * [3] [4] [5]
+     * [0] [1] [2]
+     * ```
+     * For example, if the board has the center, bottom-left, and top-right positions occupied,
+     * `self` would be `0b100010001` (binary representation of the board).
+     *
+     * @return `true` if there is a winning combination, otherwise `false`.
+     */
     function containsWin(uint256 state) internal pure returns (bool) {
+        if (state & 0x007 == 0x007) return true; // 0b000000111
+        if (state & 0x038 == 0x038) return true; // 0b000111000
+        if (state & 0x1c0 == 0x1c0) return true; // 0b111000000
+        if (state & 0x049 == 0x049) return true; // 0b001001001
+        if (state & 0x092 == 0x092) return true; // 0b010010010
+        if (state & 0x124 == 0x124) return true; // 0b100100100
+        if (state & 0x054 == 0x054) return true; // 0b001010100
+        if (state & 0x111 == 0x111) return true; // 0b100010001
         return false;
     }
 }
