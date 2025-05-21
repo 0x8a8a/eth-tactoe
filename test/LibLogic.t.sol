63268f0114d212a783dc6340b62a30e5dfaf305f
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-20 22:31:50 -0400

    test: validate reverts with LogicError(1), dirty initial state

--- test/LibLogic.t.sol
+++ test/LibLogic.t.sol
@@ -8,6 +8,14 @@ import {Test} from "forge-std/Test.sol";
 import {LibLogic} from "src/LibLogic.sol";
 
 contract LibLogicTest is Test {
+    error LogicError(uint256 errId);
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_validate_RevertsIf_TurnIsZeroAndStateIsDirty(uint16 alice, uint16 bob) public {
+        vm.expectRevert(abi.encodeWithSelector(LogicError.selector, 1));
+        LibLogic.validate(0, bound(alice, 1, 0x1ff), bound(bob, 1, 0x1ff));
+    }
+
     /// forge-config: default.allow_internal_expect_revert = true
     function test_toUint9_RevertsIf_ValueIsGreaterThan0x1ff(uint256 value) public {
         value = bound(value, 0x200, UINT256_MAX);
