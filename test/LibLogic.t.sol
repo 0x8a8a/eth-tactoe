0ca457be5a9107861b7836ebc52013f7235d5ad8
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-20 22:21:47 -0400

    test: LibLogic.toUint9 reverts with SafeCast.SafeCastOverflowedUintDowncast

--- test/LibLogic.t.sol
+++ test/LibLogic.t.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.29;
 
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+import {Test} from "forge-std/Test.sol";
+
 import {LibLogic} from "src/LibLogic.sol";
 
-contract LibLogicTest {}
+contract LibLogicTest is Test {
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_toUint9_RevertsIf_ValueIsGreaterThan0x1ff(uint256 value) public {
+        value = bound(value, 0x200, UINT256_MAX);
+
+        vm.expectRevert(abi.encodeWithSelector(SafeCast.SafeCastOverflowedUintDowncast.selector, 9, value));
+        LibLogic.toUint9(value);
+    }
+}
