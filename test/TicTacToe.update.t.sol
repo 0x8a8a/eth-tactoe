1835541f79736a2220bbd9f71020b252003e51f7
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-22 13:49:12 -0400

    fix(test): add setUp in TicTacToeUpdateTest

--- test/TicTacToe.update.t.sol
+++ test/TicTacToe.update.t.sol
@@ -3,4 +3,18 @@ pragma solidity 0.8.29;
 
 import {TicTacToeBaseTest} from "test/TicTacToe.t.sol";
 
-contract TicTacToeUpdateTest is TicTacToeBaseTest {}
+import {LibSigUtils} from "src/LibSigUtils.sol";
+
+contract TicTacToeUpdateTest is TicTacToeBaseTest {
+    using LibSigUtils for *;
+
+    function setUp() public override {
+        super.setUp();
+
+        bytes32 structHash = LibSigUtils.Open(LibSigUtils.Channel(alice, bob, 0), UINT256_MAX, 60).hash();
+        (bytes32 r, bytes32 vs) = vm.signCompact(bobPk, toTypedDataHash(structHash));
+
+        vm.prank(alice);
+        tictactoe.open(alice, bob, UINT256_MAX, 60, r, vs);
+    }
+}
