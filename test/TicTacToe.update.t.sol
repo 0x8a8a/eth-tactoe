53b948edca27131f6a4100ed8ef8c508e7e56e48
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-22 13:52:32 -0400

    test: update reverts with UnauthorizedCaller and InvalidChannel

--- test/TicTacToe.update.t.sol
+++ test/TicTacToe.update.t.sol
@@ -8,6 +8,9 @@ import {LibSigUtils} from "src/LibSigUtils.sol";
 contract TicTacToeUpdateTest is TicTacToeBaseTest {
     using LibSigUtils for *;
 
+    error UnauthorizedCaller(address caller);
+    error InvalidChannel(address alice, address bob, uint256 id);
+
     function setUp() public override {
         super.setUp();
 
@@ -16,5 +19,21 @@ contract TicTacToeUpdateTest is TicTacToeBaseTest {
 
         vm.prank(alice);
         tictactoe.open(alice, bob, UINT256_MAX, 60, r, vs);
+    }
+
+    function testFuzz_RevertsIf_CallerIsUnauthorized(address caller) public {
+        vm.assume(caller != alice && caller != bob);
+
+        vm.expectRevert(abi.encodeWithSelector(UnauthorizedCaller.selector, caller));
+
+        vm.prank(caller);
+        tictactoe.update(alice, bob, 0, 0);
+    }
+
+    function test_RevertsIf_ChannelIsNonexistent() public {
+        vm.expectRevert(abi.encodeWithSelector(InvalidChannel.selector, alice, bob, 1));
+
+        vm.prank(alice);
+        tictactoe.update(alice, bob, 1, 0);
     }
 }
