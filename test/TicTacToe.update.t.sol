82cc05b96f2530cb7e2ce22d23e7f09fb43e74a5
BaBa <210938436+0x8a8a@users.noreply.github.com>
2025-05-26 14:51:20 -0400

    fix(test): update InvalidStateTransition test in test/TicTacToe.update.t.sol

--- test/TicTacToe.update.t.sol
+++ test/TicTacToe.update.t.sol
@@ -14,7 +14,7 @@ contract TicTacToeUpdateTest is TicTacToeBaseTest {
     error UnauthorizedCaller(address caller);
     error InvalidChannel(address alice, address bob, uint256 id);
     error ExpiredChannel(uint32 expiry);
-    error InvalidStateTransition(uint16 state);
+    error InvalidStateTransition(uint32 prevState, uint32 newState);
 
     function setUp() public override {
         super.setUp();
@@ -81,13 +81,13 @@ contract TicTacToeUpdateTest is TicTacToeBaseTest {
             .depth(2)
             .checked_write(0x00010002);
 
-        vm.expectRevert(abi.encodeWithSelector(InvalidStateTransition.selector, 0x000c));
+        vm.expectRevert(abi.encodeWithSelector(InvalidStateTransition.selector, 0x00010002, 0x000c0002));
 
         vm.prank(alice);
         // alice prevState = 0b000000001, newState = 0b000001100
         // if she makes extra moves LibLogic.validate will catch it
         // on first moves, prevState = 0x0, and therefore will always pass
         // they can make any move
-        tictactoe.update(alice, bob, 0, 0x000c);
+        tictactoe.update(alice, bob, 0, 0x000c0002);
     }
 }
